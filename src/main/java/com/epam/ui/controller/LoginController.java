c3891573a7695f450b64f2e718e941164d8eda6e
Tamas Schuszter <tamas_schuszter@epam.com>
2016-07-20 14:44:52 +0200

    login faild

--- src/main/java/com/epam/ui/controller/LoginController.java
+++ src/main/java/com/epam/ui/controller/LoginController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.epam.backend.service.BankService;
 import com.epam.ui.view.UrlConstants;
@@ -20,7 +21,11 @@ public class LoginController {
 	}
 
 	@RequestMapping(value = UrlConstants.LOGIN, method = RequestMethod.GET)
-	public String login() {
+	public String login(@RequestParam(value = "login_error", required = false) Long errorCode) {
+		if (errorCode != null && errorCode == 1) {
+			return "loginFailed";
+		}
+		
 		return "login";
 	}
 }
